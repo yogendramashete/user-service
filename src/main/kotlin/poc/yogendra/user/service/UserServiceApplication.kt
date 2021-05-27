77270add1f3244d5dcac228f187b8844a5209bd5
yogendramashete <yogendra.mashete@gmail.com>
2021-05-27 16:38:59 +0530

    Initial commit

---
+++ src/main/kotlin/poc/yogendra/user/service/UserServiceApplication.kt
@@ -0,0 +1,11 @@
+package poc.yogendra.user.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserServiceApplication>(*args)
+}
---
+++ src/test/kotlin/poc/yogendra/user/service/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package poc.yogendra.user.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
